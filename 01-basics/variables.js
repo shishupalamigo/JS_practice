e87bdd1e7ce8fba8a2bf19d973dc6be833643f66
shishupalamigo <shishupalkr2595@gmail.com>
2020-07-24 11:12:36 +0530

    Initial commit

---
+++ 01-basics/variables.js
@@ -0,0 +1,15 @@
+let name = 'John';
+
+let score = 100;
+let bonus = 20;
+
+let totalScore = score + bonus;
+
+console.log(totalScore);
+
+let firstName = 'John';
+firstName = 'Jane'
+let lastName = 'Doe';
+lastName = 'Austin'
+
+console.log(firstName + ' ' + lastName)

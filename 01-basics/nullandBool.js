0b23e018aba11fe64094a1dfa48739b0fd6a59f7
shishupalamigo <shishupalkr2595@gmail.com>
2020-07-24 13:48:52 +0530

    Boolean

---
+++ 01-basics/nullandBool.js
@@ -0,0 +1,25 @@
+// temp is coming from website API 
+ 
+ var  temp 
+
+  //done some calculations 
+ // temp = 2
+
+ console.log("Current temperature is: " + temp);
+
+
+ //Grade System 
+
+ // 10 - Great job 
+ // 5 - Work Harder
+ // 2 - Poor 
+ // 0 - Fail 
+
+ // Boolean - true/false 
+
+
+ let actualMarks = 7;
+
+ let myGrade = (actualMarks <= 10);
+
+ console.log(myGrade);

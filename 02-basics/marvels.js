3d6e7435ad448f86dbd0f016cad9a421516cb68d
shishupalamigo <shishupalkr2595@gmail.com>
2020-07-28 00:34:06 +0530

    Learn Arrey

---
+++ 02-basics/marvels.js
@@ -0,0 +1,22 @@
+ // datatype - Array
+
+ const superHeros = ['Iron Man', 'Capt. America', 'Spiderman', 'Thor']
+ 
+ // we can store any type  of data in Arreys
+ // In Arreys counting starts from 0
+ 
+ 
+ console.log(superHeros);  // will print all the data of the superHeros Arrey
+
+ console.log(superHeros[0]); // will print the 1st object of the Array i.e Iron Man here
+
+ console.log(superHeros[2]); // output Spiderman
+
+ console.log(superHeros[1]); // output Capt. America
+ 
+ console.log(superHeros.length); // Will print the number of objects in the given Arrey
+
+ console.log(superHeros[superHeros.length-1]); // output Thor
+
+ console.log(`We have ${superHeros.length} super heros`); // Will print We have 4 super heros 
+ // `` can be used to do calculations in a string  

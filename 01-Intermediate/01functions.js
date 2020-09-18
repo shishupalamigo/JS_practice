868c46370165f6fc9cab24a06462d6f821407699
shishupalamigo <shishupalkr2595@gmail.com>
2020-09-18 23:54:11 +0530

    Learn default parameters

--- 01-Intermediate/01functions.js
+++ 01-Intermediate/01functions.js
@@ -18,7 +18,20 @@ let myAdder = function(num1, num2) {
     let added = num1 + num2
     return added
 }
-myAdder(5, 9)
-console.log(myAdder(5, 9)); 
+// myAdder(5, 9)
+// console.log(myAdder(5, 9)); 
 
 // functions can be called using a veriety of techniques 
+
+let myMultiplier = function(num1, num2) {
+//    let product = num1 * num2;
+//    return product;            
+      return num1 * num2;      // above two lines can be written directly as this line.     
+}
+
+let guestUser = function(name = "guest", courseCount = 0) {  // Default Parameters 
+    return "Hello " + name + ", and your course count is: " + courseCount;
+}
+// Default parameters appear only when a function is called without any arguments/parameters.
+console.log(guestUser("John", 1));
+

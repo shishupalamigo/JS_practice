45de07010fb6a795a02a3d0b0474e073ec35f0e6
shishupalamigo <shishupalkr2595@gmail.com>
2020-10-01 00:57:17 +0530

    Add assignment getMyresult app

---
+++ 01-Intermediate/assignFun.js
@@ -0,0 +1,25 @@
+let getMyGrade = function(currentMarks, totalMarks) {
+    let myPercent = (currentMarks / totalMarks) * 100
+    
+let myGrade = ''
+
+if (myPercent >= 90) {
+    myGrade = 'A'
+} else if (myPercent >= 80) {
+    myGrade = 'B'
+} else if (myPercent >= 70) {
+    myGrade = 'C'
+} else if (myPercent >= 60) {
+    myGrade = 'D'
+} else if (myPercent >= 50) {
+    myGrade = 'E'
+} else  {
+    myGrade = 'F'
+}
+   return `Your grade is ${myGrade} and your percentage is ${myPercent}`
+
+}
+
+let yourResult = getMyGrade(93, 100);
+
+console.log(yourResult);

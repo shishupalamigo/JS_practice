fd83b030a4c89350add165e8e42c153ec98c20b7
shishupalamigo <shishupalkr2595@gmail.com>
2020-10-01 00:54:23 +0530

    Learn making simple todo app

--- 01-Intermediate/trello.js
+++ 01-Intermediate/trello.js
@@ -3,15 +3,33 @@ const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 // console.log(days[0]);
 
 
-days.forEach(function (day, index) {
- //   console.log(`we work on ${index + 1} -- ${day}`);
-})
+// days.forEach(function (day, index) {
+//  //   console.log(`we work on ${index + 1} -- ${day}`);
+// })
 
 
-const months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+// const months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-let monthsName = function(month, index) { // the 1st argument is always the element of the array and second arg is always the index value.
-    console.log(`${month} is the month no. ${index + 1} of the year.`);
+// let monthsName = function(month, index) { // the 1st argument is always the element of the array and second arg is always the index value.
+//     console.log(`${month} is the month no. ${index + 1} of the year.`);
+// }
+// months.forEach(monthsName); // any function can be passeed as argument in forEach loop.
+
+// for (let index = 0; index < days.length; index++) {
+//     console.log(days[index]);
+// }
+// 
+
+const myTodo = []
+
+myTodo.push('Buy Bread');
+myTodo.push('Record video');
+myTodo.push('Go to gym');
+
+// myTodo.forEach(function(todo, i) {
+//    console.log(`Your task no. ${i + 1} is: ${todo}`);
+// })
+
+for (let i = myTodo.length - 1; i >= 0  ; i--) {
+    console.log(`Your task no. ${i + 1} is: ${myTodo[i]}`);
 }
-months.forEach(monthsName); // any function can be passeed as argument in forEach loop.
-
